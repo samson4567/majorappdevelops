2cc2db3a1b981cc359fcfbe21db7c7a7b74bdbad
samson adeyemi <samadeyemi1888@gmail.com>
2022-12-13 04:23:10 -0700

    Initial update

---
+++ lib/user_interface/SignUp.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xff33907C),
+        body: ListView(children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 100, bottom: 66),
+            child: Center(
+                child: Text(
+              "Welcome to tradly",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            )),
+          ),
+          const Center(
+              child: Text(
+            "sign up to your account",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.w400, color: Colors.white),
+          )),
+          const Padding(
+            padding: EdgeInsets.only(
+              top: 45,
+              right: 32,
+              left: 32,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'First Name',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ))),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              top: 10,
+              right: 32,
+              left: 32,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Last Name',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ))),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              top: 10,
+              right: 30,
+              left: 30,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Email ID/ Mobile Number',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ))),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              top: 10,
+              right: 30,
+              left: 30,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(24),
+                      ))),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 10, right: 30, left: 30, bottom: 30),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Re-enter Password',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(24),
+                      ))),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10, right: 30, left: 30, bottom: 30),
+            child: Container(
+              width: double.maxFinite,
+              height: 48,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(18.0))),
+              child: const Center(
+                  child: Text(
+                'Create',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              )),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+             const  Text(
+                'Have an account?',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+            ],
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: ((context) => const SignUp())));
+              },
+              child: Text(
+                "sign in",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white),
+              ))
+        ]));
+  }
+}
---
+++ lib/user_interface/homescreen.dart
@@ -0,0 +1,127 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:majorappdevelops/user_interface/SignUp.dart';
+
+class Home_screen2 extends StatelessWidget {
+  const Home_screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xff33907C),
+        body: ListView(children: [
+          const Padding(
+            padding: const EdgeInsets.only(top: 120, bottom: 66),
+            child: Center(
+                child: Text(
+              "Welcome to tradly",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            )),
+          ),
+          const Center(
+              child: Text(
+            "login to your account",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.w200, color: Colors.white),
+          )),
+          const Padding(
+            padding: EdgeInsets.only(top: 45, left: 32, right: 32),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Email/ mobile Number',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(24),
+                      ))),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 45, left: 32, right: 32),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(24),
+                      ))),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 32, right: 32, bottom: 33),
+            child: InkWell(
+              onTap: (() {}),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 40),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: double.maxFinite,
+                      height: 48,
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(18.0))),
+                      child: const Center(
+                          child: Text(
+                        'login',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      )),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 5),
+            child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'forgot your password',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                )),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            const  Text(
+                'Dont have an account ?',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+            ],
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: ((context) => const SignUp())));
+              },
+              child: Text(
+                "sign up",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white),
+              ))
+        ]));
+  }
+}
